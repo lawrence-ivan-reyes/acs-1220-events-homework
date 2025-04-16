12271b832a06608d9ab3b21638049c6d4c14569a
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2025-04-16 17:03:46 -0400

    finished models.py to-dos

--- events_app/models.py
+++ events_app/models.py
@@ -1,6 +1,18 @@
 """Create database models to represent tables."""
 from events_app import db
 from sqlalchemy.orm import backref
+from enum import Enum
+
+class EventType(Enum):
+    PARTY = 'Party'
+    STUDY = 'Study'
+    NETWORKING = 'Networking'
+    OTHER = 'Other'
+
+guest_event_table = db.Table('guest_event_table',
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True),
+    db.Column('guest_id', db.Integer, db.ForeignKey('guest.id'), primary_key=True)
+)
 
 # TODO: Create a model called `Guest` with the following fields:
 # - id: primary key
@@ -11,6 +23,17 @@ from sqlalchemy.orm import backref
 
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    phone = db.Column(db.String(20))
+    events_attending = db.relationship(
+        'Event', 
+        secondary=guest_event_table,
+        back_populates='guests'
+    )
+    
+    def __repr__(self):
+        return f'<Guest: {self.name}>'
 
 # TODO: Create a model called `Event` with the following fields:
 # - id: primary key
@@ -24,9 +47,19 @@ class Guest(db.Model):
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500))
+    date_and_time = db.Column(db.DateTime, nullable=False)
+    event_type = db.Column(db.Enum(EventType), default=EventType.OTHER)
+    guests = db.relationship(
+        'Guest', 
+        secondary=guest_event_table,
+        back_populates='events_attending'
+    )
+    
+    def __repr__(self):
+        return f'<Event: {self.title}>'
 
 # TODO: Create a table `guest_event_table` with the following columns:
 # - event_id: Integer column (foreign key)
 # - guest_id: Integer column (foreign key)
-
-guest_event_table = None
